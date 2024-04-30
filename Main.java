7e0464c366919f8b961a2c80ccac79f89acb5f09
Harish Rahukulan <159069129+harryisawesome123@users.noreply.github.com>
2024-04-29 20:35:35 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,10 +21,6 @@ class Main extends ConsoleProgram {
 
   }
 
-
-
-
-
 /* You and your date are trying to get a table at a restaurant. 
 * The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. 
 * The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes). 
@@ -191,7 +187,95 @@ public String fizzString(String str) {
   } 
  }
 
+ // Given a string, return a string where for every char in the original, there are two chars.
+
+ public String doubleChar(String str) {
+  
+  String result = "";
+
+  for (int i = 0; i < str.length(); i++) {
+    char ch;
+    ch = str.charAt(i);
+    
+    result += ch + "" + ch;
+  }
+  
+  return result;
+ }
+
+ // Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+ public int countCode(String str) {
+
+  int count = 0;
+
+  for (int i = 0; i < str.length() - 3; i++) {
+    if (str.charAt(i) == 'c' && str.charAt(i + 1) == 'o' && str.charAt(i + 3) == 'e') {
+      count++;
+    }
+  }
+  return count;
+
+ }
+
+// Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+
+public boolean bobThere(String str) {
+  
+  for (int i = 0; i < str.length() - 2; i++) {
+    if (str.charAt(i) == 'b' && str.charAt(i + 2) == 'b') {
+      return true;
+    }
+  }
+  return false;
 }
 
+// Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+public String repeatEnd(String str, int n) {
+
+  String thekinglol;
+  thekinglol = str.substring(str.length() - n);
+
+  String result = "";
+
+  for (int i = 0; i < n; i++) {
+    result += thekinglol;
+  }
+
+  return result;
+}
+
+/* Given a string, consider the prefix string made of the first N chars of the string. 
+Does that prefix string appear somewhere else in the string? Assume that the string is not empty and that N is in the range 1..str.length(). */
+
+public boolean prefixAgain(String str, int n) {
+  String prefix = "";
+  String rest = "";
+
+  prefix = str.substring(0, n);
+
+  rest = str.substring(n, str.length());
+
+  if (rest.contains(prefix)) {
+    return true;
+  }
+  return false;
+  }
 
 
+//  Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the star, they are the same.
+
+public boolean sameStarChar(String str) {
+
+  for (int i = 1; i < str.length() - 1; i++) {
+    if (str.charAt(i) == '*') {
+      if (str.charAt(i - 1) != str.charAt(i + 1)) {
+        return false;
+      }
+    }
+  }
+return true;
+
+}
+}
